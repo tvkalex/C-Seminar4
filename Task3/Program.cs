053ca5ac33e5a6888d3e8ac27edecaf2f6afd7f2
tvkalex <tvkalex@yandex.ru>
2022-12-11 20:53:21 +0300

    Add Task3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int arrLength = inputNumber("Введите размер массива: ");
+int[] arr = Auto(arrLength);
+System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+
+// Функция ввода
+int inputNumber(string message)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(message);
+        text = Console.ReadLine()!;
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+// Функция автозаполнения массива
+int[] Auto(int len)
+{
+    int[] arr = new int[len];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1, 9);
+    }
+    return arr;
+}
